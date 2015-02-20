dbd054bb1e34d60aaac48e216f9c5cd34504ce0f
Sajjan Bhattarai <sajjanbh@hotmail.com>
2015-02-20 21:19:06 +0545

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,10 +1,10 @@
 ## The main objective of these functions is to calculate the inverse of a matrix
 ## and then to cache the solution in order to prevent unnecessary processing and speed up operations.
 
-## The function 'makeMatrix()' takes the matrix as an input argument, calculates the
+## The function 'makeCacheMatrix()' takes the square invertible matrix as an input argument, calculates the
 ## inverse of the given matrix and caches it. This function outputs a list of four components.
 
-makeMatrix <- function(x = matrix()) {
+makeCacheMatrix <- function(x = matrix()) {
 	i <- NULL
 	set <- function(y) {
 		x <<- y
@@ -18,11 +18,11 @@ makeMatrix <- function(x = matrix()) {
 }
 
 
-## The function 'cacheinverse()' extracts the inverse matrix and original matrix from 
+## The function 'cacheSolve()' extracts the inverse matrix and original matrix from 
 ## the list outputted by makeMatrix() function. It evaluates the inverse matrix if
 ## it hasn't already been cached and the matrix hasn't been changed.
 
-cacheinverse <- function(x, ...) {
+cacheSolve <- function(x, ...) {
 	i <- x$getinverse()
 	if(!is.null(i) && x$get() == solve(i)) {
 		message("Getting cached data")
